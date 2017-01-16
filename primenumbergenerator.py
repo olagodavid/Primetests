587cb6b64e57fd6e0ed98ba12ebd66a5c9696041
olagodavid <olagodavid@gmail.com>
2017-01-16 17:23:24 +0300

    unittest requirements for the prime number generator function

---
+++ primenumbergenerator.py
@@ -0,0 +1,9 @@
+import unittest
+
+"""
+Test rrequirements for a function that generates prime numbers:
+ 1. n should be an interger.
+ 2. A user should be able to input a number of their choice.
+ 3. Prime numbers should be greater than 1.
+ """
+ 
