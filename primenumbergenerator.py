5b93b7fcce29b688bf441dbb29a87067ad102da2
olagodavid <olagodavid@gmail.com>
2017-01-16 23:16:47 +0300

    test requirements and a simple test case part 1

--- primenumbergenerator.py
+++ primenumbergenerator.py
@@ -6,4 +6,9 @@ Test rrequirements for a function that generates prime numbers:
  2. A user should be able to input a number of their choice.
  3. Prime numbers should be greater than 1.
  """
- 
+ class GetPrimeNumbers(object):
+ 	"""docstring for GetPrimeNumbers"""
+ 	def __init__(self, arg):
+ 		super(GetPrimeNumbers, self).__init__()
+ 		self.arg = arg
+ 		
