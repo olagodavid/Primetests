55b26e9e8bae082da37a8fbbd671b09cabac08e3
olagodavid <olagodavid@gmail.com>
2017-01-17 19:51:14 +0300

    testcases for prime number generator

--- primetests.py
+++ primetests.py
@@ -7,15 +7,17 @@ Test requirements for a function that generates prime numbers:
  3. Prime numbers should be greater than 1.
  4. If input number is less than or equal to 1, it is not prime.
  5. A user should be able to change values (upper and lower) for different results.
- 6. 
- 7.
- 8.
- 9.
- 10.
+ 6. Should contain a variable that confirms that a number is prime using a boolean value.
+ 7. Should check if a number has more factors apart from one and its self
+ 8. Should run efficiently and fast.
+ 9. Should contain a forloop to check if prime number is real
+ 10. Should be able to disprove if number is prime
  """
  class GetPrimeNumbers(object):
  	"""docstring for GetPrimeNumbers"""
- 	def __init__(self, arg):
- 		super(GetPrimeNumbers, self).__init__()
- 		self.arg = arg
+ 	def generatePrime(self):
+ 		self.assertEqual(number ** 0.5)
+ 		
+ if __name__ == '__main__':
+    unittest.main()
  		
