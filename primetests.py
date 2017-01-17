de57ccbf4dcff852a02be7b2a9afe7eac34ebf19
olagodavid <olagodavid@gmail.com>
2017-01-17 12:07:09 +0300

    list of test cases in a new separate file

--- primetests.py
+++ primetests.py
@@ -1,10 +1,17 @@
 import unittest
 
 """
-Test rrequirements for a function that generates prime numbers:
+Test requirements for a function that generates prime numbers:
  1. n should be an interger.
  2. A user should be able to input a number of their choice.
  3. Prime numbers should be greater than 1.
+ 4. If input number is less than or equal to 1, it is not prime.
+ 5. A user should be able to chnge values (upper and lower) for different results.
+ 6.
+ 7.
+ 8.
+ 9.
+ 10.
  """
  class GetPrimeNumbers(object):
  	"""docstring for GetPrimeNumbers"""
